ea283dc7799f7c7a8ca10a13c5dee057b998a64c
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2017-12-29 11:39:39 -0200

    Criação do código monolítico em ListBoxAgencias e atualização do banco de exemplo

---
+++ src/ByteBank.Agencias/ListBoxAgencias.cs
@@ -0,0 +1,26 @@
+﻿using ByteBank.Agencias.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ByteBank.Agencias
+{
+    public class ListBoxAgencias : ListBox
+    {
+        private MainWindow _janelaMae;
+
+        public ListBoxAgencias(MainWindow janelaMae)
+        {
+            _janelaMae = janelaMae ?? throw new ArgumentNullException(nameof(janelaMae));
+        }
+
+        protected override void OnSelectionChanged(SelectionChangedEventArgs e)
+        {
+            base.OnSelectionChanged(e);
+            _janelaMae.AtualizarAgenciaAtual((Agencia)SelectedItem);
+        }
+    }
+}
--- src/ByteBank.Agencias/MainWindow.xaml.cs
+++ src/ByteBank.Agencias/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ByteBank.Agencias.DAL;
 
 namespace ByteBank.Agencias
 {
@@ -20,9 +21,44 @@ namespace ByteBank.Agencias
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ByteBankEntities _db = new ByteBankEntities();
+        private ListBoxAgencias lstAgencias;
+
+        public Agencia AgenciaAtual { get; private set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            IniciarControles();
+        }
+
+        public void AtualizarAgenciaAtual(Agencia novaAgencia)
+        {
+            AgenciaAtual = novaAgencia;
+
+            Nome.Text = AgenciaAtual?.Nome;
+            Numero.Text = AgenciaAtual?.Numero;
+            Descricao.Text = AgenciaAtual?.Descricao;
+            Telefone.Text = AgenciaAtual?.Telefone;
+            Endereco.Text = AgenciaAtual?.Endereco;
+        }
+
+        private void IniciarControles()
+        {
+            lstAgencias = new ListBoxAgencias(this) { Width = 247.5, Height = 290 };
+
+            container.Children.Add(lstAgencias);
+            Canvas.SetTop(lstAgencias, 15);
+            Canvas.SetLeft(lstAgencias, 15);
+
+            AtualizarLista();
+        }
+
+        private void AtualizarLista()
+        {
+            lstAgencias.Items.Clear();
+            foreach (var item in _db.Agencias)
+                lstAgencias.Items.Add(item);
         }
     }
 }
